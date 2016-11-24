251fa36cd91eb74e88d724d6ad0d2d13aaa6638e
Justin Litchfield <j@obsidianexchange.com>
2016-11-23 19:33:17 -0600

    The Error constant is removed

--- lib/http_eventstore/api/connection.rb
+++ lib/http_eventstore/api/connection.rb
@@ -13,7 +13,7 @@ module HttpEventstore
         ) do |builder|
           builder.request :retry, max: 4, interval: 0.05,
                            interval_randomness: 0.5, backoff_factor: 2, exceptions: [
-                             Errno::ETIMEDOUT, 'Timeout::Error', Error::TimeoutError, 'HttpEventstore::ServerError'
+                             Errno::ETIMEDOUT, 'Timeout::Error', 'HttpEventstore::ServerError'
                            ]
           builder.adapter Faraday.default_adapter
           builder.response :json, content_type: APP_JSON
